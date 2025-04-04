8555d349be981065135d5a7500061291043f1924
Academy <Academy@LFA-FVFZH0F9LYWN.communityfibre.co.uk>
2025-04-04 09:45:52 +0100

    Build pseudocode for one player dice game, build basic die roll function

---
+++ index.js
@@ -0,0 +1,18 @@
+const onePlayerGame = (numRound, sidedDice) => {
+
+    // for (let i =0; i <= numRound; i++) {
+
+    // }
+    Math.floor(Math.random() * sidedDice)
+
+    // while(numRound >= 0) {
+    //     Math.floor(Math.random() * sidedDice)
+    //     if(sided) {}
+
+    // }
+
+}
+
+console.log(onePlayerGame(5, 6))
+    
+
